4913eab6711263ca7ae3945665eef763a3f39d7d
ne0lines <hermansson.david@gmail.com>
2025-12-11 21:27:35 +0100

    Refactor header structure across multiple recipe pages to remove external fetch and implement inline navigation

--- assets/scripts/script.js
+++
@@ -1,6 +0,0 @@
-fetch('https://lexicon.davidhermansson.se/recept/header.html')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('header').insertAdjacentHTML('afterbegin', html);
-    })
-    .catch(err => console.error('Failed to load header:', err));
